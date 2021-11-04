f0174509901db86f47fc87d646eadcacb1201f9e
caohaisil201 <caohaisil@gmail.com>
2021-11-04 23:48:03 +0700

    add rotate image

--- app/src/main/java/com/gnine/galleryg2/fragments/EditFragment.java
+++ app/src/main/java/com/gnine/galleryg2/fragments/EditFragment.java
@@ -4,13 +4,17 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.RotateAnimation;
+import android.widget.Button;
 import android.widget.Toolbar;
 
 import com.gnine.galleryg2.R;
+import com.makeramen.roundedimageview.RoundedImageView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,10 +23,6 @@ import com.gnine.galleryg2.R;
  */
 public class EditFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -32,20 +32,9 @@ public class EditFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EditFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static EditFragment newInstance(String param1, String param2) {
         EditFragment fragment = new EditFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -53,10 +42,6 @@ public class EditFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -65,6 +50,22 @@ public class EditFragment extends Fragment {
         // Inflate the layout for this fragment
         View view=inflater.inflate(R.layout.fragment_edit,container,false);
 //        Toolbar toolbar=getView().findViewById(R.id.toolbar);
+        RoundedImageView imageView=view.findViewById(R.id.singleImage);
+        Button closeBtn=view.findViewById(R.id.closeBtn);
+        closeBtn.setOnClickListener(v -> Navigation.findNavController(view).navigate(R.id.editToViewPagerFragment));
+        final float[] fromRotation = {0};
+        final float[] toRotation = {90};
+        Button editBtn=view.findViewById(R.id.rotateBtn);
+        editBtn.setOnClickListener(v->{
+            final RotateAnimation rotateAnim = new RotateAnimation(
+                    fromRotation[0], toRotation[0], RotateAnimation.RELATIVE_TO_SELF,.5f, RotateAnimation.RELATIVE_TO_SELF,.5f);
+
+            rotateAnim.setDuration(1000); // Use 0 ms to rotate instantly
+            rotateAnim.setFillAfter(true); // Must be true or the animation will reset
+            imageView.startAnimation(rotateAnim);
+            fromRotation[0] +=90;
+            toRotation[0] +=90;
+        });
         ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
         return view;
     }
