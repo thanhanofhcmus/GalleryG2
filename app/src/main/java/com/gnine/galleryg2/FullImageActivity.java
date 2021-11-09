9defc1077426a11a947442d7f934c24f88038b62
caohaisil201 <caohaisil@gmail.com>
2021-11-09 12:44:11 +0700

    add rotate/crop layout

--- app/src/main/java/com/gnine/galleryg2/FullImageActivity.java
+++ app/src/main/java/com/gnine/galleryg2/FullImageActivity.java
@@ -9,11 +9,17 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.gnine.galleryg2.data.ImageData;
+
+import java.util.ArrayList;
 
 public class FullImageActivity extends AppCompatActivity {
 
     public static final String IMAGE_DATA_KEY= "IMAGE_DATA_KEY";
-
+    private ArrayList<ImageData> imageDataList;
+    private int position;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +29,8 @@ public class FullImageActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         toolbar.setNavigationIcon(R.drawable.ic_back);
         toolbar.setNavigationOnClickListener(view -> finish());
+
+        getData();
     }
 
     @Override
@@ -37,11 +45,19 @@ public class FullImageActivity extends AppCompatActivity {
         if (item.getItemId() == R.id.action_favorite) {
             Toast.makeText(this, "like", Toast.LENGTH_SHORT).show();
         } else if (item.getItemId() == R.id.action_information) {
-            Toast.makeText(this, "information", Toast.LENGTH_SHORT).show();
+            ViewPager2 viewPager2=findViewById(R.id.viewPagerImageSlider);
+            int position=viewPager2.getCurrentItem();
+            Toast.makeText(this, String.valueOf(position), Toast.LENGTH_SHORT).show();
         } else if (item.getItemId() == R.id.action_aboutUs) {
             Toast.makeText(this, "About us", Toast.LENGTH_SHORT).show();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void getData(){
+        Bundle bundle= getIntent().getExtras();
+        imageDataList = bundle.getParcelableArrayList(MainActivity.IMAGE_LIST_KEY);
+        position = bundle.getInt(MainActivity.IMAGE_POSITION_KEY);
     }
 
 }
