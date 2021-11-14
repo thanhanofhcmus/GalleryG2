cab35b6efb71711198861c3698976446b21932cd
Nguyễn Thanh An <55699606+thanhanofhcmus@users.noreply.github.com>
2021-11-14 20:16:03 +0700

    Basically work

--- app/src/main/java/com/gnine/galleryg2/fragments/SettingsFragment.java
+++ app/src/main/java/com/gnine/galleryg2/fragments/SettingsFragment.java
@@ -1,16 +1,21 @@
 package com.gnine.galleryg2.fragments;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
 
 import com.gnine.galleryg2.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
+
+    SharedPreferences.OnSharedPreferenceChangeListener listener;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -24,5 +29,20 @@ public class SettingsFragment extends PreferenceFragmentCompat {
             BottomNavigationView bnv = requireActivity().findViewById(R.id.bottomNavView);
             bnv.getMenu().getItem(1).setEnabled(true);
         }
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(requireContext());
+        listener = (pref, key) -> {
+            if (key.equals("theme_mode")) {
+                String value = pref.getString(key, "INVALID");
+                if (value.equals("Light")) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                } else if (value.equals("Dark")) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+                }
+            }
+        };
+        sharedPreferences.registerOnSharedPreferenceChangeListener(listener);
     }
 }
