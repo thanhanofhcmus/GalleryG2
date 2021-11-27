b380e98225acbb1646aa7d0a148d630ee02ad4aa
Nguyễn Thanh An <55699606+thanhanofhcmus@users.noreply.github.com>
2021-11-27 22:51:09 +0700

    Replace manual UCrop with github one, change repo dependencies.

--- app/src/main/java/com/gnine/galleryg2/callback/ICropBoundsChangeListener.java
+++
@@ -1,6 +0,0 @@
-package com.gnine.galleryg2.callback;
-
-public interface ICropBoundsChangeListener {
-
-    void onCropAspectRatioChanged(float cropRatio);
-}
--- app/src/main/java/com/gnine/galleryg2/fragments/ImageViewFragment.java
+++ app/src/main/java/com/gnine/galleryg2/fragments/ImageViewFragment.java
@@ -9,13 +9,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.Toast;
 
-import com.ablanco.zoomy.TapListener;
-import com.ablanco.zoomy.Zoomy;
 import com.gnine.galleryg2.R;
-import com.gnine.galleryg2.activities.FullImageActivity;
 
 
 public class ImageViewFragment extends Fragment {
