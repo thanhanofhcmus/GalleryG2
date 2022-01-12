161ad5b1fd2bf044358402f056f8cfdc6086152f
Nguyễn Thanh An <thanhan030301@gmail.com>
2022-01-12 23:28:54 +0700

    Revert "Fix after merge"
    This reverts commit 4c48e5f9220a487c64b89011f9132782b3d5464d.

--- app/src/main/java/com/gnine/galleryg2/fragments/AllImagesFragment.java
+++ app/src/main/java/com/gnine/galleryg2/fragments/AllImagesFragment.java
@@ -29,6 +29,8 @@ import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.gnine.galleryg2.data.FolderData;
+import com.gnine.galleryg2.data.TimelineData;
 import com.gnine.galleryg2.data.RecyclerData;
 import com.gnine.galleryg2.tools.ContentHelper;
 import com.gnine.galleryg2.tools.ErrorDialog;
@@ -127,6 +129,8 @@ public class AllImagesFragment extends Fragment {
         } else {
             textView.setVisibility(View.VISIBLE);
             recyclerView.setVisibility(View.GONE);
+        }
+
         }
         updateAdapterView();
     }
